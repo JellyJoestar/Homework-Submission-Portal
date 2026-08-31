ab86fa8d91345c75b870bb238eb19ad68826a609
Jui-Chia Hsieh <chupei04@gmail.com>
2026-08-31 16:46:38 +1000

    feat(IFN636-27): configure MySQL persistence for assessments and units

---
+++ db.py
@@ -0,0 +1,16 @@
+import os
+import mysql.connector
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def get_db_connection():
+    connection = mysql.connector.connect(
+        host=os.getenv("DB_HOST"),
+        port=int(os.getenv("DB_PORT")),
+        database=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD")
+    )
+
+    return connection
